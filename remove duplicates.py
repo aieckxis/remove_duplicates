a18ce3527d8d8d6dd76dc6dd643033808cc6721d
aieckxis <aleckxiskate@gmail.com>
2023-10-01 16:18:48 +0800

    pseudocode

---
+++ remove duplicates.py
@@ -0,0 +1,7 @@
+# Define a function to remove duplicate characters from a string
+# Create an empty string to store the result
+# Iterate through the characters in the input string
+# If the character is not in the result string, add it
+# Read the number of strings (N)
+# Read the N strings
+# Process and print the strings with duplicates removed
