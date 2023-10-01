7c82aa9d7788d6322d172a2501291a695bb5a549
aieckxis <aleckxiskate@gmail.com>
2023-10-01 16:44:50 +0800

    loopt (error)

--- remove duplicates.py
+++ remove duplicates.py
@@ -3,8 +3,15 @@ def remove_duplicates(stirng):
 
     # Create an empty string to store the result
     result = ""
-# Iterate through the characters in the input string
-# If the character is not in the result string, add it
+
+    # Iterate through the characters in the input string
+    for character in string:
+
+    # If the character is not in the result string, add it
+    if character not in result:
+        result = character
+
+        return result
 # Read the number of strings (N)
 # Read the N strings
 # Process and print the strings with duplicates removed
