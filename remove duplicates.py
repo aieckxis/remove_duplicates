225557d810951e1045a14fd32285bfa6ed1f11c0
aieckxis <aleckxiskate@gmail.com>
2023-10-01 16:30:32 +0800

    defined a function and created an empty string

--- remove duplicates.py
+++ remove duplicates.py
@@ -1,5 +1,8 @@
 # Define a function to remove duplicate characters from a string
-# Create an empty string to store the result
+def remove_duplicates(stirng):
+
+    # Create an empty string to store the result
+    result = ""
 # Iterate through the characters in the input string
 # If the character is not in the result string, add it
 # Read the number of strings (N)
